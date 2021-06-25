aa0d279ccb78d77ad2c25a44955d58a0189f5863
Pingüino <Orgalorg.exe@gmail.com>
2021-06-25 05:00:50 +0200

    140 updated

--- 140.cpp
+++ 140.cpp
@@ -1,15 +1,18 @@
-#include <iostream>
-#include <string>
+//DATOS PREVIOS
+
+//includes
+#include<iostream>
 using namespace std;
 
+//Declaración de funciones
 int sumaDigitos(int n);
 
+//MAIN
 int main() {
 	int n;
 	cin >> n;
 
 	while (n > -1) {
-
 		cout << " = " << sumaDigitos(n) << endl;
 		cin >> n;
 	}
@@ -17,8 +20,16 @@ int main() {
 	return 0;
 }
 
+//IMPLEMENTACIÓN DE LA FUNCIÓN SOLUCIÓN
+//Y DETALLES DE LA SOLUCIÓN
+
+/* { Pre: 0 <= n} */
 int sumaDigitos(int n){
-	
+	//Complejidad
+	// O(n)
+	// depende del número de
+	// dígitos de n
+	 
 	//Caso base
 	if (n < 10) { 
 		cout << n;
@@ -26,7 +37,10 @@ int sumaDigitos(int n){
 	}
 
 	//LLamada recursiva
+	//Invariante
+	//
 	int ret = sumaDigitos(n / 10);
 	cout << " + " << n % 10;
 	return ret + (n % 10);
 }
+/* { Pos: } */
