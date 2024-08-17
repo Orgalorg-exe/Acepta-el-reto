6ea95bb92f07fbb0f4e88e1f5eb3d0b67c45ee34
Pingüino <Orgalorg.exe@gmail.com>
2024-08-17 07:02:59 +0200

    Update

--- problemas/378.cpp
+++ problemas/378.cpp
@@ -4,26 +4,19 @@
 #include <map>
 
 int n;
-std::map<int, int> mapa;
+//        idx           max, min
+std::map<int, std::pair<int, int>> mapa;
 
-bool justicia() {
-    std::map<int, int>::iterator it, ti;
+bool resolver() {
+    std::map<int, std::pair<int, int>>::iterator ita, itb;
+    ita = itb = mapa.begin();
+    ita++;
 
-    for (
-        it = mapa.begin(), ti = ++mapa.begin();
-        ti != mapa.end();
-        it++, ti++
-    ) {
-        if (it->second >= ti->second) return false;
+    for (; ita != mapa.end(); ita++, itb++) {
+        if (ita->second.second <= itb->second.first) return false;
     }
 
     return true;
-}
-
-std::string resolver() {
-    bool esJusta = justicia();
-    if (esJusta) return "SI";
-    return "NO";
 }
 
 bool resuelveCaso() {
@@ -31,21 +24,24 @@ bool resuelveCaso() {
 
     if (!n)
         return false;
-    
+
     mapa.clear();
     int a, b;
     for (int i = 0; i < n; i++) {
         std::cin >> a >> b;
         if (mapa.find(a) == mapa.end())
-            mapa.insert(std::pair<int, int>(a, b));
-        else
-            mapa[a] = b > mapa[a] ? b : mapa[a];
+            mapa.insert(std::pair<int, std::pair<int, int>>(a, std::pair<int, int>(b, b)));
+        else {
+            mapa[a].first = b > mapa[a].first ? b : mapa[a].first;
+            mapa[a].second = b < mapa[a].second ? b : mapa[a].second;
+        }
     }
 
-    std::string sol = resolver();
-    
-    std::cout << sol << '\n';
-    
+    bool sol = resolver();
+
+    if (sol) std::cout << "SI\n";
+    else std::cout << "NO\n";
+
     return true;
 }
 
